699f0bfb433fe07ddc3b3b21c379b8a7a53cc363
FeNriR0077 <shardul.gautam123@gmail.com>
2023-01-09 11:41:28 +0545

    Feat: Changed elements

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,14 +10,14 @@ const Todo = ({list, handleDelete, handleChange, isTrue2}) => {
             <div className="todolists">
                 
                 {list.map((li,index) => (
-                    <p className="individualitem" key={index}>
+                    <div className="individualitem" key={index}>
                         <div className="todoinfo">
                             <span>{index+1} </span>
-                            <input type='checkbox' onChange={() => {handleChange(li)}}></input> {li} <i onClick={() => {handleDelete(li)}} class="fa-solid fa-trash"></i>
+                            <input type='checkbox' onChange={() => {handleChange(li)}}></input> {li} <i onClick={() => {handleDelete(li)}} className="fa-solid fa-trash"></i>
                         </div>
 
                         <p  className={`todocompletion ${!isTrue2 ? "active" : "passive"}`}>Completed at {datestring}</p>
-                    </p>
+                    </div>
 
                     
                 ))}
